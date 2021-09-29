6e499f5e1ee15cd852700a4e9de63153709d60fa
Felix <felixyeu25@gmail.com>
2021-09-29 10:46:39 +0200

    l'algo de calcul

--- bot_discord/commands/calcul.js
+++ bot_discord/commands/calcul.js
@@ -2,14 +2,42 @@ module.exports = {
   name : 'calcul',
   description: 'calculatrice',
   execute(client, message, args){
-    message.channel.send('Quelle opération voulez-vous faire ?');
-    
+    message.channel.send('Quelle opération type d opération voulez-vous faire ?');
+    if (get.channel.message() === "add") {
+      message.channel.send('premier chiffre:');
+      a = get.channel.message();
+      message.channel.send('second chiffre:');
+      b = get.channel.message();
+      message.channel.send(a+b);
+    }
+    if (get.channel.message() === "sub"){
+      message.channel.send('premier chiffre:');
+      a = get.channel.message();
+      message.channel.send('second chiffre:');
+      b = get.channel.message();
+      message.channel.send(a-b);
+    }
+    if (get.channel.message() === "mult") {
+      message.channel.send('premier chiffre:');
+      a = get.channel.message();
+      message.channel.send('second chiffre:');
+      b = get.channel.message();
+      message.channel.send(a*b);
+    }
+    if (get.channel.message() === "div") {
+      message.channel.send('premier chiffre:');
+      a = get.channel.message();
+      message.channel.send('second chiffre:');
+      b = get.channel.message();
+      message.channel.send(a/b); 
+
+    }
   }
 } 
 
-
-const a = 12;
-const b = 3;
+let c = 0;
+let a = 0;
+let b = 0;
 
 
 const add = a+b;
